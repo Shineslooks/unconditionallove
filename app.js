f29c66718d07a743456897065bb59cffc2891630
Shineslooks <shineslooks@gmail.com>
2025-01-11 14:02:23 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -8,10 +8,7 @@ app.use(express.json()); // Middleware untuk parsing JSON
 
 // Koneksi ke MongoDB
 mongoose
-    .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    .connect(process.env.MONGO_URI)
     .then(() => console.log('Terhubung ke MongoDB Atlas'))
     .catch((err) => console.error('Kesalahan koneksi:', err));
 
